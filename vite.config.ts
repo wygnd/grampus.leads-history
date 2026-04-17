53bfb3eb65f573344541920280ae853e81d04475
wygnd <wygndo@gmail.com>
2026-04-17 17:37:38 +0300

    fix: paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueRouter from 'vue-router/vite'
-import bitrix24UIPluginVite  from '@bitrix24/b24ui-nuxt/vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueRouter from "vue-router/vite";
+import bitrix24UIPluginVite from "@bitrix24/b24ui-nuxt/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/leads-history/",
   plugins: [
     vueRouter({
-      dts: 'src/route-map.d.ts'
+      dts: "src/route-map.d.ts",
     }),
     vue(),
-    bitrix24UIPluginVite ({
+    bitrix24UIPluginVite({
       colorMode: true,
-      colorModeInitialValue: 'light',
-      colorModeTypeLight: 'light',
-      colorModeStorageKey: 'bitrix24-starter-b24ui-vue'
-    })
-  ]
-})
+      colorModeInitialValue: "light",
+      colorModeTypeLight: "light",
+      colorModeStorageKey: "bitrix24-starter-b24ui-vue",
+    }),
+  ],
+});
